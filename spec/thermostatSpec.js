3b92c42bf49d7d4b4a5a628b7a4e15efed4c9940
mongolianprincess <ayc.yang@gmail.com>
2016-02-11 13:22:09 +0000

    Added temperature up feature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,6 +9,15 @@ describe('Thermostat',function(){
   });
 
   it('starts at 20 degrees',function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('can increase the temperature with the up button',function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('',function() {
+
   });
 });
---
+++ src/thermostat.js
@@ -0,0 +1,11 @@
+function Thermostat(){
+  this.temperature = 20;
+};
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1; 
+};
